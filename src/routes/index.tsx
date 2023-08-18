0587ba0edecdc98ca2ec2110cf48798f51d556a5
dev@juliosalas.net <84051776+julio-salas03@users.noreply.github.com>
2023-08-18 00:00:11 -0500

    load data on app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,34 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { readFile } from "fs/promises";
+
+const getResourceAssets = (file: string) => {
+  return {
+    defaultAsset: file,
+    webp: file.replace(".png", ".webp"),
+    avif: file.replace(".png", ".avif")
+  }
+}
+
+export const useData = routeLoader$(async () => {
+  const loadData = async (file: string) => JSON.parse(await readFile(file, "utf-8"))
+  return {
+    characters: await loadData("./data/characters.json"),
+    synchoSkills: await loadData("./data/syncho-skills.json"),
+    inheritableSkills: await loadData("./data/inheritable-skills.json"),
+    weapons: await loadData("./data/weapons.json"),
+    engravings: await loadData("./data/engravings.json"),
+    emblems: await loadData("./data/emblems.json"),
+  }
+});
 
 export default component$(() => {
+  const data = useData()
+  console.log(data.value)
   return (
     <>
-      <picture>
-        <source srcSet="/images/characters/big/clanne.avif" type="image/avif" />
-        <source srcSet="/images/characters/big/clanne.webp" type="image/webp" />
-        <img src="/images/characters/big/clanne.png" alt="" width={100} height={100} />
-      </picture>
+      foo
     </>
   );
 });
