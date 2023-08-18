3ed2d5a3b1d6c24ad52467b991a0021c3679399c
dev@juliosalas.net <84051776+julio-salas03@users.noreply.github.com>
2023-08-18 01:38:50 -0500

    add data images to app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,13 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { readFile } from "fs/promises";
 
-// const getResourceAssets = (file: string) => {
-//   return {
-//     defaultAsset: file,
-//     webp: file.replace(".png", ".webp"),
-//     avif: file.replace(".png", ".avif")
-//   }
-// }
+const getResourceAssets = (file: string) => {
+  return {
+    defaultAsset: file,
+    webp: file.replace(".png", ".webp"),
+    avif: file.replace(".png", ".avif")
+  }
+}
 
 export const useData = routeLoader$(async () => {
   const loadData = async (file: string) => JSON.parse(await readFile(file, "utf-8"))
@@ -25,10 +25,58 @@ export const useData = routeLoader$(async () => {
 
 export default component$(() => {
   const data = useData()
-  console.log(data.value)
   return (
     <>
-      foo
+      {
+        data.value.characters.map((item: any, i: number) => {
+          const { avif, defaultAsset, webp } = getResourceAssets(item.img)
+          return <picture key={i}>
+            <source srcSet={avif} type="image/avif" />
+            <source srcSet={webp} type="image/webp" />
+            <img src={defaultAsset} alt="" width={100} height={100} />
+          </picture>
+        })
+      }
+      {
+        data.value.engravings.map((item: any, i: number) => {
+          const { avif, defaultAsset, webp } = getResourceAssets(item.img)
+          return <picture key={i}>
+            <source srcSet={avif} type="image/avif" />
+            <source srcSet={webp} type="image/webp" />
+            <img src={defaultAsset} alt="" width={100} height={100} />
+          </picture>
+        })
+      }
+      {
+        data.value.inheritableSkills.map((item: any, i: number) => {
+          const { avif, defaultAsset, webp } = getResourceAssets(item.img)
+          return <picture key={i}>
+            <source srcSet={avif} type="image/avif" />
+            <source srcSet={webp} type="image/webp" />
+            <img src={defaultAsset} alt="" width={100} height={100} />
+          </picture>
+        })
+      }
+      {
+        data.value.synchoSkills.map((item: any, i: number) => {
+          const { avif, defaultAsset, webp } = getResourceAssets(item.img)
+          return <picture key={i}>
+            <source srcSet={avif} type="image/avif" />
+            <source srcSet={webp} type="image/webp" />
+            <img src={defaultAsset} alt="" width={100} height={100} />
+          </picture>
+        })
+      }
+      {
+        data.value.weapons.map((item: any, i: number) => {
+          const { avif, defaultAsset, webp } = getResourceAssets(item.img)
+          return <picture key={i}>
+            <source srcSet={avif} type="image/avif" />
+            <source srcSet={webp} type="image/webp" />
+            <img src={defaultAsset} alt="" width={100} height={100} />
+          </picture>
+        })
+      }
     </>
   );
 });
