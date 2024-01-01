3a499336242b1ce77200b7981d4171316a9a79b6
Julio Salas <84051776+julio-salas03@users.noreply.github.com>
2024-01-01 18:57:40 -0500

    add path aliases to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import { resolve } from "path";
+
+const root = resolve(__dirname);
 
 export default defineConfig({
   plugins: [solid()],
+  resolve: {
+    alias: {
+      "@components": resolve(root, "components"),
+      "@types": resolve(root, "types"),
+      "@utils": resolve(root, "utils"),
+    },
+  },
 });
